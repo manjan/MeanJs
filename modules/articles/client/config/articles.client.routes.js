2a6dd268af8e3437496dc5c82bcac58fb2deebc4
Manish Shrestha <manish.shrestha@havasmedia.com>
2015-10-01 18:14:41 +0800

    Adding Listing Route

--- modules/articles/client/config/articles.client.routes.js
+++ modules/articles/client/config/articles.client.routes.js
@@ -15,7 +15,7 @@ angular.module('articles').config(['$stateProvider',
         templateUrl: 'modules/articles/client/views/list-articles.client.view.html'
       })
       .state('articles.listing', {
-        url: '/list',
+        url: '',
         templateUrl: 'modules/articles/client/views/list-articles.client.view.html'
       })      
       .state('articles.create', {
