09e3e1d191028eb2fb9ebd5dcdf888e9d4cc5418
Manish Shrestha <manish.shrestha@havasmedia.com>
2015-10-02 12:35:52 +0800

    Production

--- config/env/production.js
+++ config/env/production.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   port: process.env.PORT || 8443,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + ':27017/mean',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + ':27017/admin',
     options: {
       user: process.env.MONGO_USER,
       pass: process.env.MONGO_PASS
